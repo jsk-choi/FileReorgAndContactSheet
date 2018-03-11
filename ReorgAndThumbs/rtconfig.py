56f28f8035806bb9df354f630964e7f36d043db3
Joseph Choi <jsk.choi@gmail.com>
2018-03-11 12:33:42 -0700

    Fix errors

--- ReorgAndThumbs/rtconfig.py
+++ ReorgAndThumbs/rtconfig.py
@@ -3,8 +3,8 @@ def add_period_ifnot_exists(ext):
     return ext if str(ext).startswith('.') else "." + ext
 
 # reorg config
-reorg_paths = [r'\\jfs\q$\__\_new']
-thumb_paths = [r'\\jfs\q$\__\_new']
+reorg_paths = [r'\\jfs\q$\__', r'\\jfs\q$\anal', r'\\jfs\q$\feet', r'\\jfs\q$\__o\__arch\tg\_etc', r'\\jfs\vid\mov', r'\\jfs\vid\mov_4k', r'\\jfs\vid\race\Formula1', r'\\jfs\vid\race\MotoGP']
+thumb_paths = [r'\\jfs\q$\__', r'\\jfs\q$\anal', r'\\jfs\q$\feet', r'\\jfs\q$\__o\__arch\tg\_etc']
 video_ext = [".mkv", ".flv", ".avi", ".mov", ".wmv", ".mp4", ".mpg", ".mpeg", ".m2v", "m4v"]
 contact_ext = "png"
 exclude_postfix = "-zz"
